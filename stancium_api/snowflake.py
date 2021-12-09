323f2e7f342f44338141ea910e4b3ded39e6373e
Vincent <82736662+RPSMain@users.noreply.github.com>
2021-12-09 18:52:04 +0800

    Adding Asset Enums

--- stancium_api/snowflake.py
+++ stancium_api/snowflake.py
@@ -66,6 +66,8 @@ def create_snowflake(suffix: int) -> int:
 
 class SnowflakeEnum(Enum):
 	"""Enum Types"""
+	animated_asset			= -2
+	asset 					= -1
 	user 					= 0
 	servers 				= 1
 	categories				= 2
