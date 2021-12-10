72905e3b7078ea2cc0613612b06d9bf2bec6ec37
Enoki <77694265+EnokiUN@users.noreply.github.com>
2021-12-10 08:37:50 +0300

    Create encrypt.py

---
+++ stancium_api/encrypt.py
@@ -0,0 +1,44 @@
+"""
+insert loicense
+
+------------------------------
+ADDITIONAL NOTE:
+You shouldn't have access to this if you're not a member of the dev team,"
+
+If you reached it however kudos to you, now spend the next 40 months tryna decrypt ONE
+password from the database lmfao
+"""
+from hashlib import sha256
+from random import randrange, choice
+
+LETTERS = """
+qwertyuiopasdfghjklzxcvbnm
+QWERTYUIOPASDFGHJKLZXCVBNM
+1234567890
+@#$&_-()=%\"*':/!?+,.£€¥¢©®™~¿[]{}<>^¡`;÷\\|¦¬×§¶
+°あかさたなはまやらわいうおえくきこけすしそせつちとてのにぬねほひふへもみむめよ（ゆ）ろりれるわ
+・「」『』【】〔〕〒。‥…
+١٢٣٤٥٦٧٨٩٠  
+"""
+
+def gen_salt():
+	"""A function that generates random 10-30 letter long salts for encryption
+	"""
+	salt = str()
+	for _ in range(randrange(10, 31)):
+		salt += choice(LETTERS)
+	return salt
+	
+def get_hash(password, salt):
+	"""A function that returns the hex of a combination of password and salt to be used in comparison
+	or be stored in a database.
+	
+	Parameters
+	----------
+	password: :class:`str`
+		The base password supplied by the user.
+	salt: :class:`str`
+		The salt associated with the user's account
+	"""
+	to_encode = (sha256(salt[::-1].encode())[::-1] + password+salt).encode()
+	return sha256(sha256((to_encode).hexdigest()[::-1].encode()).hexdigest()[::-1]
