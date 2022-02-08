38089a50385766fd3021753ac7bc7e6b25c6ec72
Vincent <vincentbusiness55@gmail.com>
2022-02-08 21:35:30 +0800

    redo how snowflakes are made

--- stancium_api/headers.py
+++ stancium_api/headers.py
@@ -2,6 +2,6 @@ from django.http import request
 
 # initial gets
 auth = request.HttpHeaders.get("Authorization")
-reason = request.HttpHeaders.get("reason")
+reason = request.HttpHeaders.get("X-Log-Reason")
 user_agent = request.HttpHeaders.get("User-Agent")
 # checks
