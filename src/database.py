030a516e8c2724becfdcd76785aed12ede91db49
Vincent <vincentbusiness55@gmail.com>
2022-02-19 16:29:46 +0800

    chore: document the database

--- src/database.py
+++ src/database.py
@@ -24,15 +24,23 @@ dotenv.load_dotenv()
 
 client = pymongo.MongoClient(os.getenv('mongo_uri'))
 
+# databases.
 _users = client.get_database('users', read_preference=pymongo.ReadPreference.SECONDARY)
 _servers = client.get_database('servers', read_preference=pymongo.ReadPreference.SECONDARY)
 _dms = client.get_database('direct_messages', read_preference=pymongo.ReadPreference.SECONDARY)
 
+# users, core
 users = _users.get_collection('core', read_preference=pymongo.ReadPreference.SECONDARY)
+
+# servers
 members = _servers.get_collection('members', read_preference=pymongo.ReadPreference.SECONDARY)
 channels = _servers.get_collection('channels', read_preference=pymongo.ReadPreference.SECONDARY)
 messages = _servers.get_collection('messages', read_preference=pymongo.ReadPreference.SECONDARY)
 roles = _servers.get_collection('roles', read_preference=pymongo.ReadPreference.SECONDARY)
+
+# dms
 normal_dm = _dms.get_collection('normal', read_preference=pymongo.ReadPreference.SECONDARY)
 group_dm = _dms.get_collection('groups', read_preference=pymongo.ReadPreference.SECONDARY)
+
+# the servers the user is in (partial server object.)
 servers = _users.get_collection('servers')
