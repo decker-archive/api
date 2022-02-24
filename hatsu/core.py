1e12957d57b7f1d32538bf3b2667fac4d531317a
Vincent <vincentbusiness55@gmail.com>
2022-02-24 21:58:51 +0800

    feat: `GET` `/users/user-id`

--- hatsu/core.py
+++ hatsu/core.py
@@ -3,8 +3,8 @@ import dotenv
 import logging
 import quart_rate_limiter
 from quart import Quart, Response
-from .rest.guilds import channels, core
-from .rest.users import me
+from .rest.guilds import channels, core as guilds_core
+from .rest.users import me, core as users_core
 from .rest.gateway import connect
 
 app = Quart(__name__)
@@ -22,8 +22,9 @@ app.before_serving(connect)
 
 bps = {
     channels.channels: '/guilds',
-    core.guilds: '/guilds',
-    me.users_me: '/users/@me'
+    guilds_core.guilds: '/guilds',
+    me.users_me: '/users/@me',
+    users_core.users: '/users'
 }
 
 for value, suffix in bps.items():
---
+++ hatsu/rest/users/core.py
@@ -0,0 +1,33 @@
+import quart
+import json
+from ..database import users as users_db
+from ..data_bodys import error_bodys
+from ..checks import check_session_
+
+users = quart.Blueprint('users', __name__)
+
+@users.get('/<int:user_id>')
+async def get_user(user_id: int):
+    d = check_session_(quart.request.headers.get('Authorization'))
+    
+    if d == None:
+        return quart.Response(error_bodys['no_auth'], 401)
+
+    user = users_db.find_one({'id': user_id})
+
+    if user == None:
+        return quart.Response(error_bodys['invalid_data'], 400)
+    
+    # filter out non-public info, like session_ids
+    user_data = {
+        'id': user['id'],
+        'username': user['username'],
+        'separator': user['separator'],
+        'avatar_url': user['avatar_url'],
+        'banner_url': user['banner_url'],
+        'flags': user['flags'],
+        'verified': user['verified'],
+        'system': user['system'],
+    }
+
+    return quart.Response(json.dumps(user_data), 200)
